9cf808a594809231df4782805833a87f57a1abbc
Yassine Maatougui <yassinemaatougui@gmail.com>
2024-03-05 01:54:29 +0100

    Initialize DVC

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A state-of-the-art Face Sentiment Detection system that employs machine learning to accurately recognize and analyze facial expressions for emotion detection. This project utilizes the power of TensorFlow and TFLite on a Raspberry Pi, making it highly efficient for real-time applications.',
+    author='yassine maatougui',
+    license='MIT',
+)
